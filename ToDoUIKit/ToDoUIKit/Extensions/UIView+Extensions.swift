ec53164e1c83ccfb0aeeb2042d44d568cebfa4c5
Ricardo Prestes <ricardo.logan@hotmail.com>
2025-09-21 10:32:33 -0300

    Adding UIView extensions

---
+++ ToDoUIKit/ToDoUIKit/Extensions/UIView+Extensions.swift
@@ -0,0 +1,43 @@
+//
+//  UIView+Extensions.swift
+//  ToDoUIKit
+//
+//  Created by Ricardo Prestes on 21/09/25.
+//
+
+import UIKit
+
+extension UIView {
+    
+    func addSubviews(_ views: UIView...) {
+        for subview in views {
+            subview.translatesAutoresizingMaskIntoConstraints = false
+            self.addSubview(subview)
+        }
+    }
+    
+    @discardableResult
+    func top(equalTo anchor: NSLayoutYAxisAnchor, constant: CGFloat = 0) -> UIView {
+        self.topAnchor.constraint(equalTo: anchor, constant: constant).isActive = true
+        return self
+    }
+    
+    @discardableResult
+    func bottom(equalTo anchor: NSLayoutYAxisAnchor, constant: CGFloat = 0) -> UIView {
+        self.bottomAnchor.constraint(equalTo: anchor, constant: constant).isActive = true
+        return self
+    }
+    
+    @discardableResult
+    func leading(equalTo anchor: NSLayoutXAxisAnchor, constant: CGFloat = 0) -> UIView {
+        self.leadingAnchor.constraint(equalTo: anchor, constant: constant).isActive = true
+        return self
+    }
+    
+    @discardableResult
+    func trailing(equalTo anchor: NSLayoutXAxisAnchor, constant: CGFloat = 0) -> UIView {
+        self.trailingAnchor.constraint(equalTo: anchor, constant: constant).isActive = true
+        return self
+    }
+    
+}
