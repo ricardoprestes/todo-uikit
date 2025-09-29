b706d2d94b06743e92784313cb90463483660a2a
Ricardo Prestes <ricardo.logan@hotmail.com>
2025-09-29 20:04:14 -0300

    Showing data

--- ToDoUIKit/ToDoUIKit/ViewControllers/TodayTableViewController.swift
+++ ToDoUIKit/ToDoUIKit/ViewControllers/TodayTableViewController.swift
@@ -9,11 +9,53 @@ import UIKit
 
 class TodayTableViewController: UITableViewController {
 
+    let viewModel = TodayViewModel()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         view.backgroundColor = .systemBackground
+        
+        tableView.register(ItemViewCell.self, forCellReuseIdentifier: ItemViewCell.identifier)
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addItem))
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        viewModel.numberOfItems()
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: ItemViewCell.identifier, for: indexPath) as? ItemViewCell else {
+            return UITableViewCell()
+        }
+        
+        let item = viewModel.item(at: indexPath.row)
+        cell.configure(item: item)
+        
+        cell.onToggle = {[weak self] in
+            self?.viewModel.toggleDone(for: item)
+            tableView.reloadRows(at: [indexPath], with: .automatic)
+        }
+        
+        return cell
+    }
+    
+    
+    func openFormViewController(_ viewController: ToDoItemFormViewController) {
+        viewController.onSave = { [weak self]  in
+            guard let self = self else { return }
+            self.viewModel.fetchItems()
+            self.tableView.reloadData()
+        }
+        
+        let navController = UINavigationController(rootViewController: viewController)
+        present(navController, animated: true, completion: nil)
     }
 
-
+    @objc func addItem() {
+        let viewController = ToDoItemFormViewController()
+        
+        openFormViewController(viewController)
+    }
 }
